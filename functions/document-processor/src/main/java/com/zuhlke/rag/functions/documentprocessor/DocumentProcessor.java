7f85b39aa2fa94025c5c83b6ef63601236b3e5a1
Man Nguyen <minhman2202@gmail.com>
2025-04-18 15:58:52 +0700

    cleaning up proj structure

--- functions/document-processor/src/main/java/com/zuhlke/rag/functions/documentprocessor/DocumentProcessor.java
+++
@@ -1,7 +0,0 @@
-package com.zuhlke.rag.functions.documentprocessor;
-
-public class DocumentProcessor {
-    public static void main(String[] args) {
-        System.out.println("Document Processor Function");
-    }
-} 
