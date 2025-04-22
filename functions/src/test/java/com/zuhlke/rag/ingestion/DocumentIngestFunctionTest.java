a72d5e84dd232337fc67436d15c793add23cf551
Man Nguyen <minhman2202@gmail.com>
2025-04-22 08:39:45 +0700

    Setting up unit tests for the Azure functions

---
+++ functions/src/test/java/com/zuhlke/rag/ingestion/DocumentIngestFunctionTest.java
@@ -0,0 +1,13 @@
+package com.zuhlke.rag.ingestion;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class DocumentIngestFunctionTest {
+
+    @Test
+    public void testUnsupportedFileType() {
+        assertTrue(true);
+    }
+} 
---
+++ functions/src/test/java/com/zuhlke/rag/processing/IndexToAzureSearchFunctionTest.java
@@ -0,0 +1,13 @@
+package com.zuhlke.rag.processing;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class IndexToAzureSearchFunctionTest {
+
+  @Test
+    public void testUnsupportedFileType() {
+        assertTrue(true);
+    }
+}
